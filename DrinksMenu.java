ee3309b3d037a52abfcee77eec2712c46fdd6771
Tess Gaston Løvgreen <tess@lovegreen.life>
2024-03-06 12:44:27 +0100

    Drinksmenu before refactoring

--- DrinksMenu.java
+++ DrinksMenu.java
@@ -2,15 +2,21 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class DrinksMenu{
-	static TextUI textui;
+
 
   public static void main(String[] args) {
 
 
       Scanner scan = new Scanner(System.in);
-      textui = new TextUI();
 
-     int age =  textui.promptNumeric("Hvor gammel er du?");
+
+      System.out.println("Hvor gammel er du?");//Stille brugeren et spørgsmål
+      // TODO: Reducer dette til én linje ved at anvende TextUI metoden promptNumeric() i stedet for
+
+
+      String input = scan.nextLine();          //Give brugere et sted at placere sit svar og vente på svaret
+      int age = Integer.parseInt(input);       //Konvertere svaret til et tal
+
 
     /*
     Vi vil nu sammensætte en drinksmenu der afhænger af brugerens alder
@@ -32,57 +38,64 @@ class DrinksMenu{
 
       }
       options.add("vand");
-      
 
-      /* 
+
+      /*
       Vi viser listen til brugeren
       */
-      textui.displayList(options, "Her er en aldersvarende menu til dig!");
 
+      //TODO: Reducer dette til en linje ved at anvende TextUI metoden displayList() i stedet for
+      for (String option : options) {
+          System.out.println(option);
+      }
 
 
     /*
-    Vi spørger om antal af drinks, så vi ved mange gange vi skal prompte i while loopet længere nede. 
+    Vi spørger om antal af drinks, så vi ved mange gange vi skal prompte i while loopet længere nede.
     Hvert valg placerer vi i en liste, så vi kan udskrive bestillingen tilsidst.
     */
 
 
       // int numberOfDrinks = scan.nextInt();
-      // This is ok, but BEWARE OF THE SCANNERBU: When using nextInt() right before nextLine(): the nextLine call will be skipped...           
+      // This is ok, but BEWARE OF THE SCANNERBU: When using nextInt() right before nextLine(): the nextLine call will be skipped...
       // fix this by simply calling nextLine() once before you actually need it
       // Another fix: read it as string, then parse it
 
-      int numberOfDrinks= textui.promptNumeric("Hvor mange drinks vil du bestille?");
+      // TODO: anvend TextUI's promptNumeric metode, i stedet for disse to linjer
+      System.out.println("Hvor mange drinks vil du bestille?");  //Stille brugeren et spørgsmål
+      int numberOfDrinks = Integer.parseInt(scan.nextLine());    //Give brugere et sted at placere sit svar og vente på svaret
+
 
       ArrayList<String> choices = new ArrayList<String>();  //Lave en beholder til at gemme brugerens valg
-    
+
 
         while(choices.size() < numberOfDrinks){             //tjekke om brugeren skal vælge flere drinks
         //TODO: Reducer to linjer til en linje ved at anvende TextUI metoden promptChoice(options) i stedet for
-        //System.out.println("Vælg en drink fra listen: ");
-       	//String choice = scan.nextLine();
+        System.out.println("Vælg en drink fra listen: ");
+       	String choice = scan.nextLine();
 
-            String choice = textui.promptChoice(options) ;
-            choices.add(choice);
+        choices.add(choice);
    		}
 
 
 
-      /* 
+      /*
       Vi viser listen til brugeren
       */
       //TODO: Genbrug TextUI metoden displayList(choices) i stedet for
-   	  textui.displayList(choices, "Du har bestilt flg.: ");
-
+   	  System.out.println("Du har bestilt flg.: ");
+      for(String choice: choices){
+      System.out.println(choice);
+    }
 
 
 
    /* Mulige forbedringer af denne dialog:
     1. Valgmulighederne printes med et tal, så man bare taster et tal for en bestemt drink - hvor kan vi ændre på det og hvordan?
     2. Validering af at det der er blevet lagt ind i choices rent faktisk findes i menuen - som det er nu kan man bestille hvad somhelst.
-    3. Refaktorering: det hele bør ikke ligge inde i main. 
-       a. Vi bør refaktorere for at minimere gentagelser (DRY) fx. visning af lister, 
-       b. men vi bør også refaktorere for at kunne genbruge dialog mønsteret i et helt andet projekt, hvor det ikke nødvendigvis handler om drinks, men om en anden slags menu. 
+    3. Refaktorering: det hele bør ikke ligge inde i main.
+       a. Vi bør refaktorere for at minimere gentagelser (DRY) fx. visning af lister,
+       b. men vi bør også refaktorere for at kunne genbruge dialog mønsteret i et helt andet projekt, hvor det ikke nødvendigvis handler om drinks, men om en anden slags menu.
     */
 
 
@@ -90,15 +103,3 @@ class DrinksMenu{
   }
 
 }
-
-
-
-
-
-/*
-
- System.out.println("Hvor mange drinks vil du bestille?");  //Stille brugeren et spørgsmål
-      int numberOfDrinks = Integer.parseInt(scan.nextLine());    //Give brugere et sted at placere sit svar og vente på svaret
-
- */
-
